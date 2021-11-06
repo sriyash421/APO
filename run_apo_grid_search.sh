4b4c077b39930265114fa7560cf81fbe4dea0194
Kushal Kedia <kushal.k.2000@gmail.com>
2021-11-06 15:43:36 +0530

    Add environments in bash loop

--- run_apo_grid_search.sh
+++ run_apo_grid_search.sh
@@ -4,19 +4,22 @@
 
 for seed in {0..5}
 do  
-    for decay in 0.8 0.9 0.95 0.99
+    for env in HalfCheetah-v3 Ant-v3 Swimmer-v3
     do
-        for alpha in 0.03 0.1 0.3
+        for decay in 0.8 0.9 0.95 0.99
         do
-            for v in 0 0.03 0.1 0.3 1.0
+            for alpha in 0.03 0.1 0.3
             do
-                python3 -m tonic.train \
-                --header "import apo" \
-                --agent "apo.APO(alpha=${alpha}, v=${v}, trace_decay=${decay})" \
-                --environment "tonic.environments.Gym('HalfCheetah-v2')" \
-                --trainer "tonic.Trainer(test_episodes=10, epoch_steps=int(2e3), steps=int(3e6))" \
-                --seed $seed \
-                --name "APO-V_${v}-Alpha_${alpha}-TraceDecay_${decay}-Seed_${seed}" &
+                for v in 0 0.03 0.1 0.3 1.0
+                do
+                    python3 -m tonic.train \
+                    --header "import apo" \
+                    --agent "apo.APO(alpha=${alpha}, v=${v}, trace_decay=${decay})" \
+                    --environment "tonic.environments.Gym('HalfCheetah-v2')" \
+                    --trainer "tonic.Trainer(test_episodes=10, epoch_steps=int(2e3), steps=int(3e6))" \
+                    --seed $seed \
+                    --name "${env}-V_${v}-Alpha_${alpha}-TraceDecay_${decay}-Seed_${seed}" &
+                done
             done
         done
     done
--- run_ppo_grid_search.sh
+++ run_ppo_grid_search.sh
@@ -4,17 +4,20 @@
 
 for seed in {0..5}
 do  
-    for decay in 0.8 0.9 0.95 0.99
+    for env in HalfCheetah-v3 Ant-v3 Swimmer-v3
     do
-        for discount in 0.9 0.95 0.99 0.999
+        for decay in 0.8 0.9 0.95 0.99
         do
-            python3 -m tonic.train \
-            --header "import apo" \
-            --agent "apo.PPO(discount_factor=${discount}, trace_decay=${decay})" \
-            --environment "tonic.environments.Gym('HalfCheetah-v2')" \
-            --trainer "tonic.Trainer(test_episodes=10, epoch_steps=int(2e3), steps=int(3e6))" \
-            --seed $seed \
-            --name "PPO-Discount_${discount}-TraceDecay_${decay}-Seed_${seed}" &
+            for discount in 0.9 0.95 0.99 0.999
+            do
+                python3 -m tonic.train \
+                --header "import apo" \
+                --agent "apo.PPO(discount_factor=${discount}, trace_decay=${decay})" \
+                --environment "tonic.environments.Gym('${env}')" \
+                --trainer "tonic.Trainer(test_episodes=10, epoch_steps=int(2e3), steps=int(3e6))" \
+                --seed $seed \
+                --name "${env}-Discount_${discount}-TraceDecay_${decay}-Seed_${seed}" &
+            done
         done
     done
 done
